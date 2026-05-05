e8d4fe23774a8c3649cadb5a9145272b841893b4
UntoastedToaster <145788435+UntoastedToaster@users.noreply.github.com>
2026-05-05 10:40:02 +0200

    added deth to main scrren funny
    aaaaaaaaaaaaaaa56656565656565656565

--- Assets/UICoinPanel.cs
+++ Assets/UICoinPanel.cs
@@ -8,6 +8,7 @@ public class UICoinPanel : MonoBehaviour
     public TextMeshProUGUI coinText;
     public Player_CoinCount playerWealth;
     private float time = 0.9f;
+   
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,7 +18,7 @@ public class UICoinPanel : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        coinText.color = Color.Lerp(Color.white, Color.white, Mathf.Sin(Time.time)/Time.deltaTime);
+       
     }
     private void OnMoneyChanged(float newcoin, float ammountChanged)
     {
@@ -28,6 +29,7 @@ public class UICoinPanel : MonoBehaviour
 
     IEnumerator BackToWhite(float time, Action callback)
     {
+        coinText.color = Color.Lerp(Color.yellow, Color.white, Mathf.Sin(Time.time));
         yield return new WaitForSeconds(time);
         callback.Invoke();
     }
