2bd25d932696fc384745bb05e7e0c9e7899d1846
UntoastedToaster <145788435+UntoastedToaster@users.noreply.github.com>
2026-05-05 11:03:37 +0200

    fun
    aaaaaa

---
+++ Assets/randomizer.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class randomizer : MonoBehaviour
+{
+    public float min;
+    public float max;
+    public void TeleportFUN()
+    {
+        transform.position = new Vector3(3, 3, 0);
+    }
+}
